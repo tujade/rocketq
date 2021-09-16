a94665ffdf5e4dc559b41a09dfcc6c99d1311f6e
tujade <jade.mundim@gmail.com>
2021-09-16 19:54:33 -0300

    Configuração NodeJS e Início Transformação do Projeto Inicial para o NodeJS

---
+++ src/route.js
@@ -0,0 +1,9 @@
+// Rotas para o NodeJs 
+
+const express = require('express')
+
+const route = express.Router()
+
+route.get('/', (req, res) => res.render("index"))
+
+module.exports = route
---
+++ src/server.js
@@ -0,0 +1,22 @@
+// importações
+const express = require('express')
+const route = require('./route')
+const path = require('path')
+
+
+const server = express()
+
+// Define que o ejs será responsável pelo layout
+server.set('view engine', 'ejs')
+
+//Utilização pasta Public
+server.use(express.static("public"))
+
+// Mostra onde está o caminho da pasta views
+server.set('views', path.join(__dirname, 'views'))
+
+// Usa as rotas descritas no arquivo route.js
+server.use(route)
+
+//servidor que é utilizado para rodar a aplicação do Chrome
+server.listen(3000, () => console.log('Rodando!'))
