66327fb35cd3cf533af3dcb04ad435af1265c944
tujade <jade.mundim@gmail.com>
2021-09-17 20:05:21 -0300

    Ajustes EJS, Lógica JS, Criação do Banco de Dados e Inicialização BackEnd

--- src/route.js
+++ src/route.js
@@ -1,16 +1,17 @@
-//Formato que o formulário de dentro da Modal tem que passar a informação
-// route.post('/room/:room/:question/:action', (req, res) => res.render())
-
-
-// Rotas para o NodeJs 
+// Rotas da aplicação
 
 const express = require('express')
+const QuestionController = require('./controllers/QuestionController')
+const RoomController = require('./controllers/RoomController')
 
 const route = express.Router()
 
-route.get('/', (req, res) => res.render("index"))
-route.get('/create-room', (req, res) => res.render("create-room"))
-route.get('/room', (req, res) => res.render("room"))
+route.get('/', (req, res) => res.render("index", {page: 'enter-room'}))
+route.get('/create-room', (req, res) => res.render("index", {page: 'create-room'}))
+route.get('/room/:room', (req, res) => res.render("room"))
+
+route.post('/question/:room/:question/:action', QuestionController.index)
+route.post('/create-room', RoomController.create)
 
 
 module.exports = route
--- src/server.js
+++ src/server.js
@@ -2,9 +2,8 @@
 const express = require('express')
 const route = require('./route')
 const path = require('path')
-
-
 const server = express()
+
 
 // Define que o ejs será responsável pelo layout
 server.set('view engine', 'ejs')
@@ -14,6 +13,9 @@ server.use(express.static("public"))
 
 // Mostra onde está o caminho da pasta views
 server.set('views', path.join(__dirname, 'views'))
+
+// Miduere
+server.use(express.urlencoded({ extended: true}))
 
 // Usa as rotas descritas no arquivo route.js
 server.use(route)
